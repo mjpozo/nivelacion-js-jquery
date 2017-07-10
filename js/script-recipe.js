f4155884831eba766cdf55e3d42c1ecad0d61a3b
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-10 17:33:45 -0400

    Etapa 8: se llaman los botones 'make it' y 'recipe' y se les asigna el evento click. Con addClass y removeClass se agrega y se quita la clase 'make' en cada boton

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -6,5 +6,18 @@ $(document).ready(function(){
 	//ocultando menu
 	$(".js-menu").hide();
 
+	//como make ya esta en la clase del div, al apretar el boton "make it" la borramos primero y después inmediatamente añadimos la clase otra vez
+	$(".js-show-make").click(function(){
+		$(".page.recipe").removeClass("make");
+		$(".page.recipe").addClass("make");
+	});
+
+	//se quita la clase make en el boton "recipe"
+	$(".js-show-recipe").click(function(){
+		$(".page.recipe").removeClass("make");
+	});
+
+
+
 
 });
