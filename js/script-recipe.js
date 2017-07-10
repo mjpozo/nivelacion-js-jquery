d6c78480e2bf50953fab6244e397ef2185eb0fae
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-10 17:45:13 -0400

    Etapa 9: dentro de los mismos llamados del paso 8, se les agrega y se quita la clase active segun el casi, tambien con addClass y removeClass

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -6,15 +6,22 @@ $(document).ready(function(){
 	//ocultando menu
 	$(".js-menu").hide();
 
-	//como make ya esta en la clase del div, al apretar el boton "make it" la borramos primero y después inmediatamente añadimos la clase otra vez
+	//se añade la clase make en el boton "make it"
 	$(".js-show-make").click(function(){
-		$(".page.recipe").removeClass("make");
 		$(".page.recipe").addClass("make");
+
+		//clase active
+		$(".js-show-make").addClass("active");
+		$(".js-show-recipe").removeClass("active");
 	});
 
 	//se quita la clase make en el boton "recipe"
 	$(".js-show-recipe").click(function(){
 		$(".page.recipe").removeClass("make");
+
+		//clase active
+		$(".js-show-recipe").addClass("active");
+		$(".js-show-make").removeClass("active");
 	});
 
 
