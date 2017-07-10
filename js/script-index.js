91e46c506ba4b45b6ef4e9e8a95be96f7c27dd9a
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-10 13:59:45 -0400

    Etapa 4: 2. se reformulo todo lo hecho en 1, también con JQuery. El cambio es que se hizo en una sola linea

--- js/script-index.js
+++ js/script-index.js
@@ -43,19 +43,24 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
-	console.log(recipe.name);
+	console.log("id: " + recipesArray.indexOf(recipe));
+
+	//definimos una id
 
 	//creamos el html con DOM
-	$("a").addClass("item-recipe");
-	$(".item-recipe").attr("href","#");
+	$(".list-recipes").append('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">' + recipe.title + '</span><span class="metadata-recipe"><span class="author-recipe">' + recipe.source.name + '</span><span class="bookmarks-recipe"><span class="icon-bookmark"></span> </span></span></span><img src="img/recipes/320x350/' + recipe.name + '.jpg" /></a>')
+	//'<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">' + recipe.title + '</span></span></a>')//.append('<span class="title-recipe">' + recipe.title + '</span>')//.append('<span class="metadata-recipe"></span>').append('<span class="author-recipe">' + recipe.source.name + '</span>').append('<span class="bookmarks-recipe"></span>').append('<span class="icon-bookmark"></span>');
 
-	$(".item-recipe").append('<span class="attribution"></span>').append('<img src="img/recipes/320x350/' + recipe.name + '.jpg" />');
+	/*$(".item-recipe").append('<span class="attribution"></span>').append('<img src="img/recipes/320x350/' + recipe.name + '.jpg" />');
 	$(".attribution").append('<span class="title-recipe"></span>').append('<span class="metadata-recipe"></span>');
 	$(".metadata-recipe").append('<span class="author-recipe"></span>').append('<span class="bookmarks-recipe"></span>');
 	$(".bookmarks-recipe").append('<span class="icon-bookmark"></span>');
 
 	$(".title-recipe").append(recipe.title);
-	$(".author-recipe").append(recipe.source.name);
+	$(".author-recipe").append(recipe.source.name);*/
+
+	//el div con clase "list-recipes" adopta lo hecho en el DOM
+	//$(".list-recipes").append($(".item-recipe"));
 
 }
 
